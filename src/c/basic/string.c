73dd6d61cc9cb7873b54156d98079b46d91c3551
Aaron Tang <aaronwntang@gmail.com>
2021-02-09 23:09:18 -0800

    Commit basic c files

---
+++ src/c/basic/string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+// int main(int arc, string argv[])
+// {
+//   char s[3] = "HI!";  // Declaring a string as an array of characters
+
+//   if (arc == 2) {
+//     printf("Hello, %s", argv[1]);
+//   }
+
+//   return 0;
+// }
